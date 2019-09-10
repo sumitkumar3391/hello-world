42b0b650ada27a33615207408a48fa67fdf9f086
sumitkumar3391 <55125371+sumitkumar3391@users.noreply.github.com>
2019-09-10 11:02:22 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp2
+{
+    class Menudisplay
+    {
+        public int Multi()
+        {
+            Console.WriteLine("Enter First Value");
+            string value1 = Console.ReadLine();
+            Console.WriteLine("Enter Second Value");
+            string value2 = Console.ReadLine();
+            return Convert.ToInt32(value1)* Convert.ToInt32(value2);
+        }
+        
+
+        public void Read(string textFile)
+        {
+
+            string path = @"C:\Users\su.kumar1\Desktop\test.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("file Exists");
+                string text3 = File.ReadAllText(path);
+                Console.WriteLine(text3);
+            }
+            else
+            {
+                Console.WriteLine("file doesnot Exists");
+            }
+        }
+        public  List<string> Iteration()
+        {
+            List<string> ls = new List<string>(10);
+            ls.Add("one");
+            ls.Add("two");
+            ls.Add("three");
+            ls.Add("four");
+
+            return ls;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace ConsoleApp2
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            Menudisplay m1 = new Menudisplay();
+            
+            do
+                
+            {
+                Console.WriteLine("1:For Multiplication");
+                Console.WriteLine("2:For reading Text File");
+                Console.WriteLine("3:for reading properties of a c# object");
+                Console.WriteLine("4:For Exit");
+                string input = Console.ReadLine();
+                int value3 = Convert.ToInt32(input);
+
+                switch (value3)
+                {
+                    case 1:
+                        int result2 = m1.Multi();
+                        Console.WriteLine(result2);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("ENTER TEXT FILE NAME");
+                        string filename = Console.ReadLine();
+                        
+                        m1.Read(filename);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("listIteration  is ");
+                        List<string> range = m1.Iteration();
+                        foreach (string item in range)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("Bye Have a Good Day");
+                        Console.ReadLine();
+                        
+                        break;
+                      
+
+                }
+        
+
+
+
+
+
+
+
+
+
+
+
+            } while (true);
+
+
+
+
+
+        }
+    }
+}
